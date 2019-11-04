48db7db3df96b8440cd7cf6b831674f9446ab11a
MeSnek <45853458+MeSnek@users.noreply.github.com>
2019-11-04 02:41:26 -0500

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace TalkWithOtherPeopleThing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void ConnectToOtherPerson(string name, IPAddress clientIp, IPAddress hostIp, int port)
+        {
+            //listener opens on HOST ipaddress 
+            TcpListener tcpListener = new TcpListener(hostIp, port);
+            tcpListener.Start();
+            tcpListener.BeginAcceptTcpClient(null, null);
+            
+            //recipients ip goes in client
+            TcpClient myTcpClient = new TcpClient();
+            myTcpClient.BeginConnect(clientIp, port, null, null);
+            System.Threading.Thread.Sleep(5000);
+            if (myTcpClient.Connected)
+            {
+                MessageBox.Show("You're connected!");
+            }
+            else
+            {
+                MessageBox.Show("You weren't able to connect");
+                System.Windows.Application.Current.Shutdown();
+            }
+            //todo: actually send messages lol
+        }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WelcomeWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TalkWithOtherPeopleThing
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class WelcomeWindow : Window
+    {
+        public WelcomeWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string name = NameTextBox.Text;
+            IPAddress clientIp = IPAddress.Parse(IpTextBox.Text);
+            IPAddress hostIp = IPAddress.Parse(HostIpTextBox.Text);
+            Int32 port = Int32.Parse(PortTextBox.Text);
+
+            BeginConnection(name, clientIp, hostIp, port);
+        }
+        private void BeginConnection(string name, IPAddress clientIp, IPAddress hostIp, int port)
+        {   
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.ConnectToOtherPerson(name, clientIp, hostIp, port);
+            mainWindow.Show();
+            this.Close();
+        }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void IpTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
